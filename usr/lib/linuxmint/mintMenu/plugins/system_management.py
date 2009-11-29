7f851b48d85f09e548d2193c259ab7c487ad3474
William Witt <william@witt-family.net>
2009-11-29 10:38:05 -0500

    fixed terminal not launching from the system_management plugin

--- usr/lib/linuxmint/mintMenu/plugins/system_management.py
+++ usr/lib/linuxmint/mintMenu/plugins/system_management.py
@@ -153,7 +153,7 @@ class pluginclass( object ):
 		
 		if ( self.showTerminal == True ):
 			Button4 = easyButton( "gnome-terminal", self.iconsize, [_("Terminal")], -1, -1 )
-			Button4.connect( "clicked", self.ButtonClicked, "x-terminal-emulator" )
+			Button4.connect( "clicked", self.ButtonClicked, "gnome-terminal" )
 			Button4.show()
 			self.systemBtnHolder.pack_start( Button4, False, False )
 			self.mintMenuWin.setTooltip( Button4, _("Use the command line") )
